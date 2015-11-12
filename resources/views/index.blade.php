ed7d6ae198b77efd6f93a4ad0aaefd5d99eb52ca
Mo Moulton <mo.moulton22@gmail.com>
2015-11-12 17:54:01 -0500

    master layout sticky footer and route

---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.master')
+
+@section('content')
+    <h3>Here will be the main stuff!</h3>
+@stop
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html>
+<head>
+    <title>
+        {{-- Yield the title if it exists, otherwise default to 'Mo Moulton' --}}
+        @yield('title', 'Mo Moulton')
+    </title>
+
+    <meta charset='utf-8'>
+    <link href="/css/style.css" type='text/css' rel='stylesheet'>
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+
+
+    {{-- Yield any page specific CSS files or anything else you might want in the <head> --}}
+    @yield('head')
+
+</head>
+<body>
+
+    <header>
+        <h1>Mo Moulton</h1>
+    </header>
+
+    <section>
+        {{-- Main page content will be yielded here --}}
+        @yield('content')
+    </section>
+
+    <footer>
+      <div>
+        <nav>
+          <ul>
+            <li><a href="https://twitter.com/hammock_tussock" target='_blank'>Twitter</a></li>
+            <li><a href="https://github.com/momoulton" target='_blank'>GitHub</a></li>
+            <li><a href="https://www.linkedin.com/pub/mo-moulton/a1/98b/2b9" target='_blank'>LinkedIn</a></li>
+            <li>    <a href = "mailto:moulton2ATfasDOTharvardDOTedu"
+       onclick = "this.href=this.href
+                  .replace(/AT/,'&#64;')
+                  .replace(/DOT/,'&#46;')
+                  .replace(/DOT/,'&#46;')"
+            >Contact Me</a></li>
+            <li><a href="/">Home</a></li>
+          </ul>
+        </nav>
+      </div>
+
+    </footer>
+
+    {{-- Yield any page specific JS files or anything else you might want at the end of the body --}}
+    @yield('body')
+
+</body>
+</html>
