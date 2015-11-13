156e723f20df20c848ae5a7b65e979048c857090
Mo Moulton <mo.moulton22@gmail.com>
2015-11-12 21:09:46 -0500

    rudimentary routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,3 +12,35 @@
 */
 
 Route::get('/', 'HomeController@getIndex');
+
+Route::get('/about', function() {
+    return "All about me";
+});
+
+Route::get('/books', function() {
+    return "book time";
+});
+
+Route::get('/articles', function() {
+    return "article time";
+});
+
+Route::get('/web', function() {
+    return "web stuff";
+});
+
+Route::get('/sayers', function() {
+    return "DLS here!";
+});
+
+Route::get('/ireland', function() {
+    return "eire";
+});
+
+Route::get('/britain', function() {
+    return "uk uk uk";
+});
+
+Route::get('/fun', function() {
+    return "some kinda something";
+});
